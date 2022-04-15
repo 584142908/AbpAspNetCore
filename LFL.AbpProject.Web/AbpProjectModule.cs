d8233943b65b33e782277b72830dd3bd194123ac
lfl <584142908@qq.com>
2022-04-15 10:04:24 +0800

    项目初始化导入

--- LFL.AbpProject.Web/AbpProjectModule.cs
+++ LFL.AbpProject.Web/AbpProjectModule.cs
@@ -27,7 +27,6 @@ namespace LFL.AbpProject.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
